2a6db82ec3d2cf4aea7bee947cedfe6699e9e315
aellien <ellien@iap.fr>
2025-06-23 16:07:29 +0200

    synth out2

--- launch_jobs_synthesis.sh
+++ launch_jobs_synthesis.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-for file in /n03data/ellien/Euclid_LSB_DR1/data/EUC_EDF_N_bin4_2.5k2/*2693*_scaled.cut*.fits
+for file in /n03data/ellien/Euclid_LSB_DR1/data/EUC_EDF_N_bin4_2.5k2/*_scaled.cut*.fits
 do
   echo "Launch synthesis for file ${file}"
   n=$(basename "$file")
-  bash start_synthesis_slurm.sh $n
+  sbash start_synthesis_slurm.sh $n
 done
