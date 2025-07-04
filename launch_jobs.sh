dfa69dcbcd48ed23cb187d8f2aa88845bb89e5cb
aellien <ellien@iap.fr>
2025-07-04 14:32:58 +0200

    out3

--- launch_jobs.sh
+++ launch_jobs.sh
@@ -3,8 +3,8 @@ for file in /n03data/ellien/Euclid_LSB_DR1/data/EUC_EDF_N_bin4_2.5k2/*_scaled.cu
 do
   #echo "Launch Dawis on file ${file}"
   n=$(basename "$file")
-  idtile=${file:18:4}
-  idcut=$(echo $file | sed -n 's/.*cut\([0-9]\+\)\.fits/\1/p') # à l'arrache
+  idtile=${n:18:4}
+  idcut=$(echo $n | sed -n 's/.*cut\([0-9]\+\)\.fits/\1/p') # à l'arrache
   echo "/n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits"
   if [ -f /n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits ] 
   then
