61101b94b5641948c1a22996d969faa7b407a78f
aellien <ellien@iap.fr>
2025-07-04 14:28:59 +0200

    out3

--- .ipynb_checkpoints/launch_jobs-checkpoint.sh
+++ .ipynb_checkpoints/launch_jobs-checkpoint.sh
@@ -5,7 +5,8 @@ do
   n=$(basename "$file")
   idtile=${file:18:4}
   idcut=$(echo $file | sed -n 's/.*cut\([0-9]\+\)\.fits/\1/p') # à l'arrache
-  if [ -f /n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits ] then
+  if [ -f /n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits ] 
+  then
     echo "bash start_dawis_slurm.sh $n"
   fi
 done
