044b6b59e9afbb55cde88eca8b87dfeace1c92f4
aellien <ellien@iap.fr>
2025-07-04 14:38:23 +0200

    out3

--- .ipynb_checkpoints/launch_jobs-checkpoint.sh
+++ .ipynb_checkpoints/launch_jobs-checkpoint.sh
@@ -6,7 +6,7 @@ do
   idtile=${n:18:4}
   idcut=$(echo $n | sed -n 's/.*cut\([0-9]\+\)\.fits/\1/p') # à l'arrache
   echo "/n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits"
-  if [ -f /n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits ] 
+  if [ -f /n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.log.txt ] 
   then
     #echo "bash start_dawis_slurm.sh $n"
     echo "found"
