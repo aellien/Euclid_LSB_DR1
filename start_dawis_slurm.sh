83fa23234fd7428651056a884b4b799a7c9946ba
aellien <ellien@iap.fr>
2025-07-04 14:41:22 +0200

    out3

--- start_dawis_slurm.sh
+++ start_dawis_slurm.sh
@@ -6,8 +6,8 @@ sbatch <<EOT
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=48:00:00
-#SBATCH --output /n03data/ellien/Euclid_LSB_DR1/logs/%x.%j.out 
-#SBATCH --error  /n03data/ellien/Euclid_LSB_DR1/logs/%x.%j.err
+#SBATCH --output /n03data/ellien/Euclid_LSB_DR1/logs/%x.%j.id$2_cut$3.out 
+#SBATCH --error  /n03data/ellien/Euclid_LSB_DR1/logs/%x.%j.id$2_cut$3.err
 
 source /home/ellien/.bashrc
 conda activate dawis
