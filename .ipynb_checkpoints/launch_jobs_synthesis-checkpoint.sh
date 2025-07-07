a71f27b0b88398139e26c2790898a485c9e7a92a
aellien <ellien@iap.fr>
2025-07-07 10:56:20 +0200

    out3 synth

--- .ipynb_checkpoints/launch_jobs_synthesis-checkpoint.sh
+++ .ipynb_checkpoints/launch_jobs_synthesis-checkpoint.sh
@@ -3,5 +3,12 @@ for file in /n03data/ellien/Euclid_LSB_DR1/data/EUC_EDF_N_bin4_2.5k2/*_scaled.cu
 do
   echo "Launch synthesis for file ${file}"
   n=$(basename "$file")
-  bash start_synthesis_slurm.sh $n
+  idtile=${n:18:4}
+  idcut=$(echo $n | sed -n 's/.*cut\([0-9]\+\)\.fits/\1/p') # à l'arrache
+  echo "/n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits"
+  if [ -f /n08data/ellien/Euclid_LSB_DR1/wavelets/out3/${idtile}/EUC_VIS_LSB_ObsID_${idtile}_scaled.cut${idcut}.synth.lvl_sep.fits ] 
+  then
+    echo "synthesis image found, passing"
+  else
+      bash start_synthesis_slurm.sh $n
 done
