46a161129a0198b76b4f01a90b0a305907ab1c61
aellien <ellien@iap.fr>
2025-08-23 13:03:33 +0200

    out4

--- dawis_input.py
+++ dawis_input.py
@@ -5,7 +5,7 @@ import shutil
 import cProfile
 from datetime import datetime
 
-indir = '/n03data/ellien/Euclid_LSB_DR1/data/test'
+indir = '/n03data/ellien/Euclid_LSB_DR1/data/EUC_EDF_N_bin4_2.5k2'
 infile = sys.argv[1]
 tile_id = infile.split('_')[4]
 outdir = '/n08data/ellien/Euclid_LSB_DR1/wavelets/out4/%s/'%tile_id
